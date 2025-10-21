e9886621805b36b163d8c1f47ca177294df0fb29
Cleniq Danes <131790951+cdanescmd@users.noreply.github.com>
2025-10-21 13:36:50 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Toggle navbar for mobile
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+// Highlight current section in navbar
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => link.classList.remove('active'));
+      document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+    }
+  });
+};
